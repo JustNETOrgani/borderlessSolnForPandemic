70ae42f1af675ac2111cb77f7d9290e8056cd811
JustNET <justnetonline@outlook.com>
2021-07-25 22:46:39 +0800

    Base page for country level done

---
+++ src/views/Country_level/countryLanding.vue
@@ -0,0 +1,146 @@
+<template>
+    <div class="pageContainer">
+        <div id="topNav">
+          <el-link icon="el-icon-arrow-left" style="font-size:17px;float:left;" @click="backToPrvPg">Previous Page</el-link>
+        </div>
+        <div class="wrapper" :loading="pageLoadingState">
+            <h3>Country Management Dashboard</h3>
+            <el-row>
+                <el-col :span="6">
+                    <h5>Name of Country: {{country_Name}}</h5>
+                </el-col>
+                <el-col :span="6">
+                    <h5>Address of Smart Contract: {{country_SCaddr}}</h5>
+                </el-col>
+                <el-col :span="6">
+                    <h5>IPFS hash of TC document: {{country_tcIPFShash}}</h5>
+                </el-col>
+            </el-row>
+            <div class="tasksSections" id="registTask">
+                <el-row>
+                    <el-col :span="12" :offset="5">
+                        <h4>Registration</h4>
+                        <a href="registerTC"><img class="coreTasksLogo" src="../../assets/imgs/registerCountry.png" /></a>
+                        <br>
+                        <el-link :underline="false" href="registerTC"><p>Register Testing Center (TC)</p></el-link>
+                    </el-col>
+                </el-row>
+            </div>
+            <div class="tasksSections" id="actTasks">
+                <el-row>
+                    <el-col :span="12" :offset="5">
+                        <h4>Activations</h4>
+                    </el-col>
+                </el-row>
+                <el-row>
+                    <el-col :span="6" :offset="4">
+                        <a href="reactivateTC"><img class="coreTasksLogo" src="../../assets/imgs/reactivate.png" /></a>
+                        <br>
+                        <el-link :underline="false" href="reactivateTC"><p>Reactivate TC</p></el-link>
+                    </el-col>
+                    <el-col :span="6" :offset="4">
+                        <a href="revokeTC"><img class="coreTasksLogo" src="../../assets/imgs/revokeCountry.png" /></a>
+                        <br>
+                        <el-link :underline="false" href="revokeTC"><p>Revoke TC</p></el-link>
+                    </el-col>
+                </el-row>
+            </div>
+            <div class="tasksSections" id="statsTasks">
+                <el-row>
+                    <el-col :span="12" :offset="5">
+                        <h4>Statistics</h4>
+                        <a href="statistics"><img class="coreTasksLogo" src="../../assets/imgs/statisticsImg.png" /></a>
+                        <br>
+                        <el-link :underline="false" href="statistics"><p>Retrieve test/vaccine statistics</p></el-link>
+                    </el-col>
+                </el-row>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      pageLoadingState: false,
+      country_Name: '',
+      country_SCaddr: '',
+      country_tcIPFShash: ''
+    }
+  },
+  created () {
+    /* if (!ethEnabled()) {
+      this.$message('Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!')
+    } else {
+      this.getAccount().then(accounts => {
+        this.currentAddress = accounts[0]
+        console.log('Current account: ', this.currentAddress)
+      })
+    } */
+    this.pageLoadingState = true
+    // Make a call to WHO SC to get details about country before page loads. Name, Deployed SC address.
+  },
+  methods: {
+    backToPrvPg () {
+      this.$router.push('/')
+    },
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped>
+#pageContainer {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+#topNav{
+  height: 3%;
+}
+h3{color:cornflowerblue}
+h4{text-align: center; color: darksalmon;}
+
+.wrapper {
+  background-color: #ffffff;
+  border-radius: 4px;
+  margin: 2.5% auto;
+  width: 90%;
+  padding: 1rem 1.5rem;
+}
+.tasksSections{
+    width: 33%;
+    height: 35%;
+    float: left;
+    align-content: center;
+    margin-top: 1.4rem;
+}
+
+#registTask{
+    background-color: rgb(203, 214, 214);
+}
+#actTasks{
+    background-color: rgb(198, 228, 245);
+}
+#statsTasks{
+    background-color: rgb(203, 214, 214);
+}
+.coreTasksLogo{
+  height: 5rem;
+  border-radius: 50%;
+  width: 5em;
+}
+.coreTasksLogo:hover{
+    transform: scale(1.1); /* Negative value rotates and decreases size.*/
+}
+</style>
---
+++ src/views/Country_level/login.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="home" :style="image" id="bgImage">
+    <div id="topNav">
+        <el-link icon="el-icon-arrow-left" style="font-size:17px;float:left;" @click="BackToPrvPage">Previous</el-link>
+    </div>
+    <div id="loginWrapper">
+      <el-row>
+      <el-col :span="22">
+        <el-col :span="6">
+          <fieldset>
+            <legend>Country Login</legend>
+              <img alt="MetaMask Logo" src="../../assets/imgs/metaMaskLogo.png">
+              <el-button :loading="metaMaskLoginBtn" @click="metaMaskLogin()" type="primary" plain>Login with MetaMask</el-button>
+          </fieldset>
+        </el-col>
+      </el-col>
+    </el-row>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import ethEnabled from '@/assets/js/web3nMetaMask'
+import web3 from '@/assets/js/web3Only'
+import { ABI, contractAddress, suppliedGas } from '@/assets/js/ABIs/WHO_ABI'
+// Import image.
+// import Bg2 from '@/../assets/images/logo2.png'
+
+export default {
+  // name: 'Home',
+  data () {
+    return {
+      currentAddress: '',
+      accountChangeStatus: false,
+      // Loading states
+      metaMaskLoginBtn: false,
+      // Background image.
+      image: { backgroundImage: 'url(https://user-images.githubusercontent.com/44321289/126899606-29af81b1-65ac-4492-a70a-813eed16f359.jpg)' }
+    }
+  },
+  components: {
+  },
+  created () {
+    /* if (!ethEnabled()) {
+      this.$message('Please install an Ethereum-compatible browser or extension like MetaMask to use this dApp!')
+    } else {
+      this.getAccount().then(accounts => {
+        this.currentAddress = accounts[0]
+        console.log('Current account: ', this.currentAddress)
+      })
+    } */
+  },
+  watch: {
+    'currentAddress' () {
+      this.switchAccount()
+    }
+  },
+  methods: {
+    async getAccount () {
+      var accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      return accounts
+    },
+    switchAccount () {
+      var myRoot = this // Ensure all this or vue global variables can be accessed within this fucntion via myRoot.
+      window.ethereum.on('accountsChanged', function (accounts) {
+        myRoot.currentAddress = accounts[0]
+        console.log('Selected account: ', myRoot.currentAddress)
+        myRoot.$message({
+          message: 'Account switched successfully.',
+          type: 'success'
+        })
+        console.log('Account switched')
+        myRoot.accountChangeStatus = true
+      })
+    },
+    metaMaskLogin () {
+      console.log('Attempting MetaMask login via the SC...')
+      this.metaMaskLoginBtn = true
+      var countrySC = new web3.eth.Contract(ABI, contractAddress, { defaultGas: suppliedGas })// End of ABi Code from Remix.
+      console.log('Contract instance created.')
+      // Smart contract and other logic continues.
+      countrySC.methods.checkLoginAddr().call({ from: this.currentAddress }).then(res => {
+        if (res === true) {
+          // Login success. Proceed.
+          this.$message({
+            message: 'Login success',
+            type: 'success'
+          })
+          this.metaMaskLoginBtn = false
+          this.$router.push('/Country_level/countryLanding')
+        } else {
+          // Failed authentication.
+          this.$message.error('Sorry! Failed login.')
+          this.metaMaskLoginBtn = false
+        }
+      })
+    },
+    BackToPrvPage () {
+      this.$router.push('/')
+    }
+  },
+  computed: {
+
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+#bgImage {
+        background-repeat: no-repeat;
+        background-size: 100% auto;
+    }
+#topNav{
+  width: 100%;
+  height: 5%;
+}
+#loginWrapper{
+  margin: 17% 40%;
+  width: 20%;
+  padding: 1rem 1.5rem;
+}
+fieldset{
+    border-radius: 10%;
+}
+legend {
+ font-style: italic;
+ color: rgb(243, 235, 232);
+ font-size: 1.3rem;
+}
+</style>
