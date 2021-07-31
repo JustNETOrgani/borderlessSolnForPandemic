c582ed4f61dee0bd701d2bbcb9369ec7b2803c41
JustNETlab <justnetonline@outlook.com>
2021-08-01 01:22:08 +0800

    Signature modified

--- src/assets/js/sigHelperFns.js
+++ src/assets/js/sigHelperFns.js
@@ -1,26 +1,22 @@
 import Web3 from 'web3'
-var ethUtil = require('ethereumjs-util')
 
 async function signatureGen (data, accountToUse, callback) {
-  var msg = ethUtil.bufferToHex(Buffer.from(data))
+  // var msg = ethUtil.bufferToHex(Buffer.from(data))
   var from = accountToUse
   console.log('Sending personal sign request to MetaMask')
-  var params = [msg, from]
-  var method = 'personal_sign'
+  var params = [data, from]
+  // var method = 'personal_sign'
   window.web3 = new Web3(window.ethereum)
-  window.web3.currentProvider.sendAsync({
-    method,
-    params,
-    from
-  }, function (err, result) {
+  window.web3.eth.personal.sign(params[0], params[1], function (err, result) {
     if (err) return console.log('Signature error: ', err)
     if (result.error) return console.log('Sorry! Error with signature result: ', result.error)
     // console.log('PERSONAL SIGNED:' + JSON.stringify(result.result))
-    callback(JSON.stringify(result.result))
+    callback(JSON.stringify(result))
   })
 }
 
-signatureGen('data', 'accountToUse', function (sig) {
+// On page load sample input params.
+signatureGen('data', 'accnt', function (sig) {
   console.log('Signature: ', sig)
 })
 
